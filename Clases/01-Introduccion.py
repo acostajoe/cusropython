847002ed3e8c174097fbeb09e669c7307c338879
joe <121851669+acostajoe@users.noreply.github.com>
2024-08-14 21:13:11 -0400

    Update 01-Introduccion.py

--- Clases/01-Introduccion.py
+++ Clases/01-Introduccion.py
@@ -1,11 +1,2 @@
 mensaje = "Hola mundo"
 print(type(mensaje))
-
-#clase: es el plano de construccion
-#objeto: una instancia de una clase
-
-#clase: es el plano de construccion de una casa
-#objeto: la casa construida
-
-#clase: Humano
-#objeto: Nicolas, Felipe, Maria, Juan
