99f9e8cf6c9cfb77acde2f757bdd2515bce36a80
Joel Acosta <joecosta9911@gmail.com>
2024-08-14 21:09:13 -0400

    Clases

---
+++ Clases/01-Introduccion.py
@@ -0,0 +1,11 @@
+mensaje = "Hola mundo"
+print(type(mensaje))
+
+#clase: es el plano de construccion
+#objeto: una instancia de una clase
+
+#clase: es el plano de construccion de una casa
+#objeto: la casa construida
+
+#clase: Humano
+#objeto: Nicolas, Felipe, Maria, Juan
